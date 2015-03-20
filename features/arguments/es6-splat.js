607f5c212d2fb4d3ab5f43a7c508aa5a6e118a52
Benjamin Lupton <b@lupton.cc>
2015-03-20 18:28:36 +0800

    arguments to reset-parameters

--- features/arguments/es6-splat.js
+++
@@ -1,5 +0,0 @@
-// --harmony-rest-parameters (iojs does not yet support this)
-"use strict"
-module.exports = function(...args){
-	return
-}
---
+++ features/rest-parameters/es6-splat.js
@@ -0,0 +1,8 @@
+// --harmony-rest-parameters (iojs does not yet support this)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parameters
+// https://code.google.com/p/v8/issues/detail?id=2159
+// https://groups.google.com/forum/embed/?place=forum/strengthen-js#!topic/strengthen-js/2lW_VzHBfKw
+"use strict"
+module.exports = function(...args){
+	return
+}
